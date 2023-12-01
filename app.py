8b875764ed1ed1173dbbcbce89ed7ecf6fe4e93d
Carolina Moura <91756302+carolinamouraf@users.noreply.github.com>
2023-12-01 14:58:12 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,206 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from aux import *
+from PIL import Image
+
+# Creating side bar
+
+image = Image.open('../assets/farmbits-logo-alt2.png')
+st.sidebar.image(image)
+st.sidebar.markdown('# Pesquisar novo produto:')
+
+product = st.sidebar.text_input('Insira o produto')
+
+price_farm = st.sidebar.text_input('Preço Farmbits')
+
+help_str = 'Adicionar o produto na lista de itens que devem ficar salvos para futuras pesquisas. Caso não deseje salvar, desative a opção antes de procurar pelo item.'
+on = st.sidebar.checkbox(label='Salvar produto', value = False, key='saving_prod', help = help_str)
+
+dictionary_items_prices = open_item_list()
+st.sidebar.markdown('# Pesquisar produtos salvos:')
+option = st.sidebar.selectbox(
+    'Produtos', dictionary_items_prices.keys())
+
+if on:
+    save_item(product, price_farm)
+
+butt2 = st.sidebar.button('Buscar produto')
+
+if butt2:
+    product = option
+    price_farm = dictionary_items_prices[option]
+    print(price_farm)
+    try:
+        price_farm = price_farm.replace(',', '.')
+        price_farm = float(price_farm)
+    except:
+        pass
+    
+else:
+    price_farm = price_farm.replace(',', '.')
+    price_farm = float(price_farm)
+
+# Displaying the data
+
+st.subheader('Produtos encontrados:')
+
+# Running the crawlers and reading the dataframes
+df_merc = mercado_livre(product)
+if type(df_merc) == str:
+    str_result_merc = 'Produto não encontrado no site Mercado Livre!'
+else:
+    #df_merc.to_csv("../data/merc_livre_" + str(product) + ".csv", index = False)
+    pass
+
+df_agros = agrosolo(product)
+if type(df_agros) == str:
+    str_result = 'Produto não encontrado no site Agrosolo!'
+else:
+    #df_agros.to_csv("../data/agrosolo_" + str(product) + ".csv", index = False)
+    pass
+    
+df_lojagro = loja_agropecuaria(product)
+if type(df_lojagro) == str:
+    str_result_lojagro = 'Produto não encontrado no site Loja Agropecuária!'
+else:
+    #df_lojagro.to_csv("../data/loja_agropecuaria_" + str(product) + ".csv", index = False)
+    pass
+
+max_prices = []
+min_prices = []
+
+#df_mercado_livre = pd.read_csv("../data/merc_livre_" + str(product) + ".csv")
+try:
+    df_merc['PRECO_DESCONTO'] = df_merc['PRECO_DESCONTO'].str.replace('.', '')
+    df_merc['PRECO_DESCONTO'] = df_merc['PRECO_DESCONTO'].str.replace(',', '.')
+    df_merc['PRECO_DESCONTO'] = df_merc['PRECO_DESCONTO'].astype(float)
+except:
+    #df_merc['PRECO_DESCONTO'] = df_merc['PRECO_DESCONTO'].replace('.','')
+    df_merc['PRECO_DESCONTO'] = df_merc['PRECO_DESCONTO'].replace(',','.')
+    df_merc['PRECO_DESCONTO'] = df_merc['PRECO_DESCONTO'].astype(float)
+mean_price_merc_livre = df_merc['PRECO_DESCONTO'].mean()
+df_red_livre_max = df_merc[df_merc['PRECO_DESCONTO'] == df_merc['PRECO_DESCONTO'].max()]
+df_red_livre_min = df_merc[df_merc['PRECO_DESCONTO'] == df_merc['PRECO_DESCONTO'].min()]
+df_price_merc_livre = df_red_livre_max.append(df_red_livre_min).reset_index(drop = True)
+df_price_merc_livre['LOJA'] = 'Mercado Livre'
+
+if type(df_agros) != str:
+    #df_agros = pd.read_csv("../data/agrosolo_" + str(product) + ".csv")
+    try:
+        df_agros['PRECO_DESCONTO'] = df_agros['PRECO_DESCONTO'].str.replace('.','')
+        df_agros['PRECO_DESCONTO'] = df_agros['PRECO_DESCONTO'].str.replace(',','.')
+        df_agros['PRECO_DESCONTO'] = df_agros['PRECO_DESCONTO'].astype(float)
+    except:
+        df_agros['PRECO_DESCONTO'] = df_agros['PRECO_DESCONTO'].replace(',','.')
+        df_agros['PRECO_DESCONTO'] = df_agros['PRECO_DESCONTO'].astype(float)
+
+    if type(df_agros['PRECO_ORIGINAL']) == str:
+        df_agros['PRECO_ORIGINAL'] = df_agros['PRECO_ORIGINAL'].str.replace(',','.')
+        df_agros['PRECO_ORIGINAL'] = df_agros['PRECO_ORIGINAL'].astype(float)
+    else:
+        pass
+    mean_price_agrosolo = df_agros['PRECO_DESCONTO'].mean()
+    mean_price_agrosolo = df_agros['PRECO_DESCONTO'].mean()
+    df_red_agrosolo_max = df_agros[df_agros['PRECO_DESCONTO'] == df_agros['PRECO_DESCONTO'].max()]
+    df_red_agrosolo_min = df_agros[df_agros['PRECO_DESCONTO'] == df_agros['PRECO_DESCONTO'].min()]
+    df_price_agrosolo = df_red_agrosolo_max.append(df_red_agrosolo_min).reset_index(drop = True)
+    df_price_agrosolo['LOJA'] = 'Agrosolo'
+else:
+    mean_price_agrosolo = 0
+    df_price_agrosolo = pd.DataFrame(columns=['PRODUTO','PRECO_DESCONTO','DESCONTO_%','FORMA_PAGAMENTO','PRECO_ORIGINAL','DESCONTO','LOJA'])
+
+if type(df_lojagro) != str:
+# df_lojagro = pd.read_csv("../data/loja_agropecuaria_" + str(product) + ".csv")
+    try:
+        df_lojagro['PRECO_DESCONTO'] = df_lojagro['PRECO_DESCONTO'].str.replace('.','')
+        df_lojagro['PRECO_DESCONTO'] = df_lojagro['PRECO_DESCONTO'].str.replace(',','.')
+        df_lojagro['PRECO_DESCONTO'] = df_lojagro['PRECO_DESCONTO'].astype(float)
+    except:
+        df_lojagro['PRECO_DESCONTO'] = df_lojagro['PRECO_DESCONTO'].replace(',','.')
+        df_lojagro['PRECO_DESCONTO'] = df_lojagro['PRECO_DESCONTO'].astype(float)
+
+    mean_price_lojagro = df_lojagro['PRECO_DESCONTO'].mean()
+    mean_price_lojagro = round(mean_price_lojagro, 2)
+    mean_price_lojagro = df_lojagro['PRECO_DESCONTO'].mean()
+    mean_price_lojagro = round(mean_price_lojagro, 2)
+    df_red_lojagro_max = df_lojagro[df_lojagro['PRECO_DESCONTO'] == df_lojagro['PRECO_DESCONTO'].max()]
+    df_red_lojagro_min = df_lojagro[df_lojagro['PRECO_DESCONTO'] == df_lojagro['PRECO_DESCONTO'].min()]
+    df_price_lojagro = df_red_lojagro_max.append(df_red_lojagro_min).reset_index(drop = True)
+    df_price_lojagro['LOJA'] = 'Loja Agropecuária'
+
+else:
+    mean_price_lojagro = 0
+    df_price_lojagro = pd.DataFrame(columns=['PRODUTO','PRECO_DESCONTO','DESCONTO_%','FORMA_PAGAMENTO','PRECO_ORIGINAL','DESCONTO','LOJA'])
+
+mean_price = (mean_price_merc_livre + mean_price_agrosolo + mean_price_lojagro)/3
+mean_price = round(mean_price, 2)
+difference_to_mean = price_farm - mean_price
+difference_to_mean = round(difference_to_mean, 2)
+
+df_prices = pd.DataFrame(columns = ['PRODUTO','PRECO_DESCONTO','DESCONTO_%','FORMA_PAGAMENTO','PRECO_ORIGINAL','DESCONTO', 'LOJA'])
+df_prices = df_prices.append(df_price_merc_livre).reset_index(drop = True)
+df_prices = df_prices.append(df_price_agrosolo).reset_index(drop = True)
+df_prices = df_prices.append(df_price_lojagro).reset_index(drop = True)
+
+#max_value = max(max_prices)
+df_max = df_prices[df_prices['PRECO_DESCONTO'] == df_prices['PRECO_DESCONTO'].max()]
+max_prod = df_max.iloc[0, 0]
+max_value = float(df_max.iloc[0, 1])
+#max_value = float(df_max['PRECO_DESCONTO'])
+store_max = df_max.iloc[0, 6]
+difference_to_max = price_farm - max_value
+difference_to_max = round(difference_to_max, 2)
+
+#min_value = min(min_prices)
+df_min = df_prices[df_prices['PRECO_DESCONTO'] == df_prices['PRECO_DESCONTO'].min()]
+min_prod = df_min.iloc[0, 0]
+min_value = float(df_min.iloc[0, 1])
+store_min = df_min.iloc[0, 6]
+difference_to_min = price_farm - min_value
+difference_to_min = round(difference_to_min, 2)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Média de preços", mean_price, difference_to_mean)
+col2.metric("Maior preço", max_value, difference_to_max)
+col3.metric("Menor preço", min_value, difference_to_min)
+
+st.markdown('**Maior preço:**')
+st.markdown(max_prod)
+st.markdown('**Loja com maior preço:**')
+st.markdown(store_max)
+st.markdown('**Menor preço:**')
+st.markdown(min_prod)
+st.markdown('**Loja com menor preço:**')
+st.markdown(store_min)
+
+if type(df_merc) == str:
+    st.markdown('#### Mercado Livre')
+    st.write(str_result_merc)
+else:
+    st.markdown('#### Mercado Livre')
+    st.dataframe(df_merc)
+    #st.download_button(label="Download", data=csv, file_name='mercado_livre' + str(product) + '.csv')
+
+
+if type(df_agros) == str:
+    st.markdown('#### Agrosolo')
+    st.write(str_result)
+else:
+    #df_agrosolo = pd.read_csv("../data/agrosolo_" + str(product) + ".csv")
+    st.markdown('#### Agrosolo')
+    st.dataframe(df_agros)
+
+
+if type(df_lojagro) == str:
+    st.markdown('#### Loja Agropecuária')
+    st.write(str_result_lojagro)
+else:
+    #df_lojagro = pd.read_csv("../data/loja_agropecuaria_" + str(product) + ".csv")
+    st.markdown('#### Loja Agropecuária')
+    st.dataframe(df_lojagro)
+
+
+
+    
+
---
+++ aux.py
@@ -0,0 +1,427 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import pandas as pd
+import numpy as np
+
+
+
+def save_item(product, price_farm):
+    with open('../data/saved_products.txt', 'a') as file:
+        file.write(str(product) + "@" + str(price_farm) + '\n')
+        # adicionar opção de checar se o produto já existe no arquivo e, caso exista, oferecer substituição
+        # OBS: criar função de atualizar txt
+
+def open_item_list():
+    search_dic = {}
+    with open('../data/saved_products.txt', 'r') as file:
+        items_list = file.readlines()
+
+        for line in items_list:
+            splitted = line.split('@')
+            if len(splitted) == 2:
+                prod = splitted[0]
+                price = splitted[1]
+
+                price = price.replace(',','.')
+                price = float(price)
+                
+                search_dic[prod] = price
+
+    return search_dic
+
+
+def mercado_livre(product):
+    '''Responsible for getting the product's prices and payment conditions from mercado livre's website'''
+    
+    def create_url(key_word):
+        key_word = str(key_word)
+        url_search = key_word.replace(" ", "%20")
+        search = key_word.replace(" ", "-")
+
+        url = "https://lista.mercadolivre.com.br/" + str(search) + "#D[A:" + str(url_search) + "]"
+
+        return url
+
+    
+    # Accessing the section on mercado livre's website
+
+    url = create_url(product)
+    page = requests.get(url)
+    
+    # Creating the soup object
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    # Get the iformations from the classes: name, price, discount and payment method
+
+    class1 = "ui-search-item__title"
+    class2 = "andes-money-amount ui-search-price__part ui-search-price__part--medium andes-money-amount--cents-superscript"
+    class3 = "ui-search-price__discount"
+    class4 = "ui-search-item__group__element ui-search-installments ui-search-color--BLACK"
+    class5 = "ui-search-item__group__element ui-search-installments ui-search-color--LIGHT_GREEN"
+    
+    name_disc = []
+    for link in soup.find_all(class_=[class1, class2, class3, class4, class5]): #finding the names at the title class in the code
+        info = link.get_text()
+        name_disc.append(info)
+        
+    i = 0
+    for item in name_disc:
+        info_payment = item.replace('em', '$em$')
+        info_payment = info_payment.split('$')
+
+        if np.isin('em', info_payment) == False:
+            info_price = item.replace('$', '-$-')
+            info_price = info_price.split('-')
+
+            if np.isin('$', info_price) == True:
+                name_disc[i] = item + str('|price|')
+                i = i+1 
+            else:
+                i = i+1
+                pass
+        else:
+            i = i+1
+            pass
+     
+    # Creating a dictionary assigning the informations
+    info_dict = {}
+    for item in name_disc:
+
+        info_payment = item.replace('em', '$em$')
+        info_payment = info_payment.split('$')
+
+        info_price = item.split('|')
+
+        info_discount = item.split(' ')
+
+        if (np.isin('em', info_payment) == False) & (np.isin('OFF', info_discount) == False) & (np.isin('price', info_price) == False):
+            if item in info_dict.keys():
+                item_change = item + str(' OPÇÃO_2')
+                info_dict[item_change] = ['price', 'discount', 'payment']
+                prev_item = item_change
+            else:
+                info_dict[item] = ['price', 'discount', 'payment']
+                prev_item = item
+
+        elif np.isin('price', info_price) == True:
+            price = info_price[0].split("$")
+            try:
+                info_dict[prev_item][0] = price[1]
+            except:
+                pass
+            
+        elif np.isin('OFF', info_discount) == True:
+            splitted = item.split('%')
+            try:
+                info_dict[prev_item][1] = splitted[0]
+            except:
+                pass
+            
+        elif np.isin('em', info_payment) == True:
+            try:
+                info_dict[prev_item][2] = item
+            except:
+                pass
+            
+            
+    # Constructing the dataframe with the data
+    if len(info_dict) == 0:
+        df_infos = "nulo"
+    else:
+        df = pd.DataFrame.from_dict(info_dict, orient='index')
+        df_infos = df.reset_index()
+        df_infos = df_infos.rename(columns = {'index':'PRODUTO', 0:'PRECO_DESCONTO', 1:'DESCONTO_%', 2:'FORMA_PAGAMENTO'})
+        df_infos['DESCONTO_%'] = df_infos['DESCONTO_%'].replace('discount', 0)
+        df_infos['PRECO_ORIGINAL'] = 0
+    
+        for l in df_infos.index:
+            discount = df_infos['DESCONTO_%'][l]
+            price_dic = df_infos['PRECO_DESCONTO'][l]
+            if discount != 0:
+                num_disc = int(discount)
+                price_dic = price_dic.replace('.','')
+                price_final = price_dic.replace(',','.')
+                num_price_disc = float(price_final)
+
+                num_payment = 100 - num_disc
+                original_price = (num_price_disc * 100)/num_payment
+
+                df_infos['PRECO_ORIGINAL'][l] = original_price
+                
+            else:
+                pass
+    
+    return df_infos
+
+def agrosolo(product):
+    
+    def create_url(key_word):
+        key_word = str(key_word)
+        url_search = key_word.replace(" ", "%20")
+        url = "https://www.agrosolo.com.br/busca?busca=" + str(url_search)
+
+        return url
+
+    # Accessing the section on mercado livre's website
+
+    url = create_url(product)
+    page = requests.get(url)
+    
+    # Creating the soup object
+    soup = BeautifulSoup(page.text, 'html.parser') 
+    
+    class1 = 'spots__title' #product name
+    class2 = 'product__price--installment' # price with PIX
+    class3 = 'product__price--after' # price with credit card
+    class4 = 'product__off uppercase flex align-center justify-center' #  discount
+    class5 = 'product__price--before' # price before discount
+    
+    name_disc = []
+    for link in soup.find_all(class_=[class1,class2,class3,class5]): #finding the names at the title class in the code
+        info = link.get_text()
+        name_disc.append(info)
+        
+        
+    i = 0
+    for item in name_disc:
+        info_payment = item.replace('PIX', '$PIX$')
+        info_payment = info_payment.split('$')
+
+        if np.isin('PIX', info_payment) == False:
+            info_card = item.replace('ou', '-ou-')
+            info_card = info_card.split('-')
+
+            if np.isin('ou', info_card) == False:
+                price_before = item.replace('$', '-$-')
+                price_before = price_before.split('-')
+
+                if np.isin('$', price_before) == True:
+                    name_disc[i] = item + str('|price_before|')
+                    i = i+1 
+                else:
+                    i = i+1
+                    pass
+            else:
+                i = i+1
+                pass
+        else:
+            i = i+1
+            pass
+        
+    info_dict = {}
+    for item in name_disc:
+
+        info_payment = item.replace('PIX', '-PIX-')
+        info_payment = info_payment.split('-')
+
+        price_before = item.split('|')
+
+        info_card = item.replace('ou', '-ou-')
+        info_card = info_card.split('-')
+
+        if (np.isin('PIX', info_payment) == False) & (np.isin('price_before', price_before) == False) & (np.isin('ou', info_card) == False):
+            if item in info_dict.keys():
+                item_change = item + str(' OPÇÃO_2')
+                info_dict[item_change] = ['price_now', 'discount', 'credit_card', 'price_before']
+                prev_item = item_change
+            else:
+                info_dict[item] = ['price_now', 'discount', 'credit_card', 'price_before']
+                prev_item = item
+
+        elif np.isin('PIX', info_payment) == True:
+            splitted = item.split(' ')
+            first = splitted[1]
+            if first != 'R$':
+                info_dict[prev_item][0] = splitted[1]
+            else:
+                info_dict[prev_item][0] = splitted[2]
+            
+        elif np.isin('price_before', price_before) == True:
+            price_before = price_before[0]
+            splitted = price_before.split(' ')
+            first = splitted[0]
+            if first != 'R$':
+                info_dict[prev_item][3] = first
+            else:
+                info_dict[prev_item][3] = splitted[1]
+            
+        elif np.isin('ou', info_card) == True:
+            info_dict[prev_item][2] = item
+    
+    prod_discount = []
+    for link in soup.find_all(class_=[class1,class4]): #finding the names at the title class in the code
+        info = link.get_text()
+        prod_discount.append(info)
+         
+    i = 0
+    for item in prod_discount:
+        info = item.split('%')
+        if np.isin(' off', info) == True:
+            product = prod_discount[i+1]
+            info_dict[product][1] = info[0]
+            i = i+1
+        else:
+            i = i+1
+            pass
+        
+    # Creating the dataframe
+    if len(info_dict) == 0:
+        df_infos = "nulo"
+
+    else:
+        df = pd.DataFrame.from_dict(info_dict, orient='index')
+        df_infos = df.reset_index()
+        df_infos = df_infos.rename(columns = {'index':'PRODUTO', 0:'PRECO_DESCONTO', 1:'DESCONTO_%', 2:'FORMA_PAGAMENTO', 3:'PRECO_ORIGINAL'})
+        df_infos['DESCONTO_%'] = df_infos['DESCONTO_%'].replace('discount', 0)
+        df_infos['DESCONTO_%'] = df_infos['DESCONTO_%'].astype(float)
+        df_infos['PRECO_ORIGINAL'] = df_infos['PRECO_ORIGINAL'].replace('price_before', 0)
+
+        mask = df_infos['PRECO_DESCONTO'] == 'price_now'
+        df_infos = df_infos[~mask]
+    
+    
+    return df_infos
+
+def loja_agropecuaria(product):
+
+    def create_url(key_word):
+        key_word = str(key_word)
+        url_search = key_word.replace(" ", "%20")
+        
+        url = "https://www.lojaagropecuaria.com.br/" + str(url_search)
+
+        return url
+    
+    # Accessing the section on loja agropecuaria's website
+
+    url = create_url(product)
+    page = requests.get(url)
+
+    # Creating the soup object
+    soup = BeautifulSoup(page.text, 'html.parser') 
+
+    divs = []
+    name_disc = []
+    for link in soup.find_all(class_='desc position-relative'): #finding the names at the title class in the code
+        info = link.get_text()
+        name_disc.append(info)
+
+    for item in name_disc:
+        info_list = item.split('\n')
+        divs.append(info_list)
+
+    dic_infos = {}
+
+    for item in divs:
+        product_name = item[1].split('                            ')
+        product_name = product_name[1]
+        dic_infos[product_name] = ['price', 'payment']
+        
+        product_price = item[2].split('                        ')[1].split(' ')[1].split('ou')[0]
+        dic_infos[product_name][0] = product_price
+        
+        payment = item[2].split('                        ')[1].split('ou')[1]
+        dic_infos[product_name][1] = payment
+
+    # Creating the dataframe
+
+    if len(dic_infos) == 0:
+        df_infos = "nulo"
+    else:
+        df = pd.DataFrame.from_dict(dic_infos, orient='index')
+        df_infos = df.reset_index()
+        df_infos['DESCONTO_%'] = 0
+        df_infos['PRECO_ORIGINAL'] = 0
+        df_infos = df_infos.rename(columns = {'index':'PRODUTO', 0:'PRECO_DESCONTO', 1:'FORMA_PAGAMENTO'})
+        df_infos = df_infos[['PRODUTO', 'PRECO_DESCONTO', 'DESCONTO_%', 'FORMA_PAGAMENTO', 'PRECO_ORIGINAL']]
+
+        for i in df_infos.index:
+            info = df_infos['PRECO_DESCONTO'][i]
+            splitted = info.split('R')
+            if np.isin('$', splitted) == True:
+                price = splitted[0]
+                df_infos['PRECO_DESCONTO'][i] = price
+            else:
+                pass
+
+        #df_infos["PRECO_DESCONTO"]=df_infos["PRECO_DESCONTO"].str.replace(',','.')
+        #df_infos["PRECO_DESCONTO"]=df_infos["PRECO_DESCONTO"].astype(float)
+    
+    return df_infos
+
+'''def loja_mecanico(product):
+
+    def create_url(key_word):
+        key_word = str(key_word)
+        url_search = key_word.replace(" ", "%20")
+
+        url = "https://www.lojadomecanico.com.br/busca?q=" + str(url_search)
+
+        return url
+    
+    # Accessing the section on mercado livre's website
+    url = create_url("alicate")
+    page = requests.get(url)
+
+    # Creating the soup object
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    name_disc = []
+    for link in soup.find_all(class_= 'body-card flex-grow-1'): #finding the names at the title class in the code
+        info = link.get_text()
+        name_disc.append(info)
+
+    info_dict = {}
+
+    for item in name_disc:
+        splitted_infos = item.split('\n')
+        prod_name = splitted_infos[7].split('                 ')
+        prod_name = prod_name[0]
+        prod_name = prod_name.split('                ')[1]
+        info_dict[prod_name] = ['current_price', 'discount', 'old_price', 'payment_cond']
+        
+        old_price = splitted_infos[13].split(' ')
+        if np.isin('DE:', old_price) == True:
+            old_price = old_price[2]
+            info_dict[prod_name][2] = old_price
+        else:
+            current_price = old_price[1]
+            info_dict[prod_name][0] = current_price
+            
+        try:
+            discount = splitted_infos[18].split(' ')
+            discount = discount[32]
+            info_dict[prod_name][1] = discount
+        except:
+            payment_cond = splitted_infos[17].split('                    ')[1]
+            info_dict[prod_name][3] = payment_cond
+        
+        current_price = splitted_infos[21].split(' ')
+        if np.isin('R$', current_price) == True:
+            current_price = current_price[1]
+            info_dict[prod_name][0] = current_price
+        else:
+            pass
+        
+        try:
+            payment_cond = splitted_infos[25].split(' ')
+            if np.isin('cartão', payment_cond) == True:
+                payment_cond = splitted_infos[25].split('                    ')[1]
+                info_dict[prod_name][3] = payment_cond
+            else:
+                pass
+            
+        except:
+            pass
+
+    
+    # Creating the dataframe
+    df = pd.DataFrame.from_dict(info_dict, orient='index')
+    df_infos = df.reset_index()
+    df_infos = df_infos.rename(columns = {'index':'PRODUTO', 0:'PRECO_DESCONTO', 1:'DESCONTO_%', 2:'PRECO_ORIGINAL', 3:'FORMA_PAGAMENTO'})
+    df_infos = df_infos[['PRODUTO', 'PRECO_DESCONTO', 'DESCONTO_%', 'FORMA_PAGAMENTO', 'PRECO_ORIGINAL']]
+    df_infos['DESCONTO_%'].replace('discount', 0, inplace=True)
+    df_infos['PRECO_ORIGINAL'].replace('old_price', 0, inplace=True)
+
+    return df_infos'''
